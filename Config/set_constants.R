ca97032d453f8878fec9754278c2d9448d246ca9
awp-finanznachrichten <sw@awp.ch>
2024-11-21 12:54:52 +0100

    commit from Rstudio

--- Config/set_constants.R
+++ Config/set_constants.R
@@ -5,7 +5,7 @@ date_voting <- "20241124"
 
 #Save texts? Simulation? Default FALSE
 save_texts <- FALSE
-simulation <- TRUE
+simulation <- FALSE
 
 #Mail
 DEFAULT_MAILS <- "contentdevelopment@keystone-sda.ch, robot-notification@awp.ch"
